b2d16b6ff2ee26d07845a6c26e7fbb61bcd1957d
Hannah Mark <hannah.f.mark@gmail.com>
2020-06-29 13:41:34 -0500

    back to the old version

--- do_cor.sh
+++ do_cor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=12
-#Seed2Cor parameters.txt
-/P/hmark/bin/Seed2Cor/src/Seed2Cor parameters_new.txt > junk_cor
+Seed2Cor parameters.txt > junk_cor
+#/P/hmark/bin/Seed2Cor/src/Seed2Cor parameters_new.txt > junk_cor
