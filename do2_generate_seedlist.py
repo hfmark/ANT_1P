10a27a261416bf916c641436ea501be11237d331
Hannah Mark <hannah.f.mark@gmail.com>
2020-06-30 11:14:51 -0500

    clean up network-specific listings since dayfiles now include all networks

--- do2_generate_seedlist.py
+++ do2_generate_seedlist.py
@@ -5,7 +5,7 @@ import os, sys
 
 ####
 # write a list of [path yyyy mm dd] for seedfiles to input to seed2cor
-# include both 1P/C/C1/G station files and ENAP seed files (once those exist)
+# dayfiles include 1P/C/C1/G/ENAP all catted together
 ####
 
 # open seedlist file for writing
@@ -17,34 +17,6 @@ flist = glob('/P/hmark/ANT_1P/seed/dayfiles/*.seed')
 # for each, get y/m/d, glob seed files, and write
 for i in range(len(flist)):
 	date = '.'.join(flist[i].split('/')[-1].split('_')[-1].split('.')[:3])
-	d = datetime.strptime(date,'%Y.%m.%d')
-	fs.write('%s  %s  %02d  %02d\n' % (flist[i],str(d.year),d.month,d.day))
-
-fs.close()
-sys.exit()
-
-
-
-
-
-
-
-
-
-# 1P etc
-flist = glob('/P/hmark/ANT_1P/seed/1P/*/*.seed')
-
-# for each, get y/m/d, glob seed files, and write
-for i in range(len(flist)):
-	date = '.'.join(flist[i].split('/')[-2].split('_')[-1].split('.')[:3])
-	d = datetime.strptime(date,'%Y.%b.%d')
-	if d.year == 2018 and d.month < 8:
-		fs.write('%s  %s  %02d  %02d\n' % (flist[i],str(d.year),d.month,d.day))
-
-# ENAP:
-flist = glob('/P/hmark/ANT_1P/seed/ENAP/*.seed')
-for i in range(len(flist)):
-	date = flist[i].split('/')[-1].split('_')[-1][:10]
 	d = datetime.strptime(date,'%Y.%m.%d')
 	fs.write('%s  %s  %02d  %02d\n' % (flist[i],str(d.year),d.month,d.day))
 
