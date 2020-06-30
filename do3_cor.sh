d42a11b22ff32d8f3f4baf27d788453ac670b558
Hannah Mark <hannah.f.mark@gmail.com>
2020-06-30 11:53:57 -0500

    clean up, add reminder to start cor since stdout and stderr are redirected

--- do3_cor.sh
+++ do3_cor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=12
+echo 'remember to enter y to start correlation calculation'
 Seed2Cor parameters.txt > junk_cor
-#/P/hmark/bin/Seed2Cor/src/Seed2Cor parameters_new.txt > junk_cor
