b569070bb6791ea5ce4530997b3bc9c80bbb9945
Hannah Mark <hannah.f.mark@gmail.com>
2020-07-03 15:01:13 -0500

    number of threads

--- do3_cor.sh
+++ do3_cor.sh
@@ -3,4 +3,4 @@
 export OMP_NUM_THREADS=12
 echo 'remember to enter y to start correlation calculation'
 #Seed2Cor parameters.txt > junk_cor
-/P/hmark/bin/Seed2Cor/Seed2Cor parameters_new.txt > junk_cor
+/P/hmark/bin/Seed2Cor/Seed2Cor parameters_new.txt 12 > junk_cor
