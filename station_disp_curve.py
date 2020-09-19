8d1f1be95471822b7ec96b66b7e7416a7f75296c
Hannah Mark <hannah.f.mark@gmail.com>
2020-09-19 15:26:02 -0500

    extract and plot dispersion curves at each station location

---
+++ station_disp_curve.py
@@ -0,0 +1,77 @@
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import mods.PAT.anttomo as ant
+from obspy import read_inventory
+from matplotlib.backends.backend_pdf import PdfPages
+import pickle
+import os, sys
+
+####
+# read maps from EQ and ANT tomo, read stations, get disp curves for station locations
+####
+
+# read in EQ tomography outputs
+aphv = sio.loadmat(os.path.expanduser('~/Patagonia/EQ_tomo/matgsdf-patagonia/helmholtz_stack_LHZ.mat'),variable_names='avgphv')
+aphv = aphv['avgphv']
+emaps = {aphv[:,i]['period'][0][0][0]:ant.EQVelocityMap(aphv[:,i]) for i in range(len(aphv[0]))}
+
+# read in ANT outputs
+f = open('output/3-pass-tomography_phase.pickle','rb')
+vmaps = pickle.load(f)
+f.close()
+
+f = open('output/3-pass-tomography_group.pickle','rb')
+vmaps_group = pickle.load(f)
+f.close()
+
+opdf = '../Plots/disp_curve_stations.pdf'
+pdf = PdfPages(opdf)
+
+# read inventory, get station info (coords/names for all stations)
+inv = read_inventory('seed/dataless/combined.xml')
+stnm = np.array([e.split(' ')[0].split('.')[-1] for e in inv.get_contents()['stations']])
+slat = np.array([inv.select(station=e).networks[0].stations[0].latitude for e in stnm])
+slon = np.array([inv.select(station=e).networks[0].stations[0].longitude for e in stnm])
+
+# periods for both
+eper = [emaps[k].period for k in emaps.keys()]
+aper = [vmaps[k].period for k in vmaps.keys()]
+gper = [vmaps_group[k].period for k in vmaps_group.keys()]
+
+plt.ioff()
+# for a station, get velocities from ANT and EQ and plot both? to see how they compare?
+for i in range(len(stnm)):
+    try:
+        evel = [emaps[k].interp_velocity(slon[i],slat[i])[0] for k in emaps.keys()]
+        estd = [emaps[k].interp_velocity(slon[i],slat[i])[1] for k in emaps.keys()]
+        avel = [vmaps[k].interp_velocity(slon[i],slat[i]) for k in vmaps.keys()]
+        astd = [max(2./vmaps[k].density_interp(slon[i],slat[i])[0], 0.05) for k in vmaps.keys()]
+        astd = [min(0.2,e) for e in astd]
+        gvel = [vmaps_group[k].interp_velocity(slon[i],slat[i]) for k in vmaps_group.keys()]
+        # NOTE this assumes set of ant phase periods includes all of the group periods
+        gstd = [max(2./vmaps[k].density_interp(slon[i],slat[i])[0], 0.05) for k in vmaps_group.keys()]
+        gstd = [min(0.2,e) for e in gstd]
+        gstd = 2.5*np.array(gstd)
+    except:
+        print(stnm[i])
+        continue  # probably a bounds error for interpolation
+
+    fig = plt.figure()
+    #plt.plot(aper, avel, '.-', label='ambient, phase')
+    plt.errorbar(aper, avel, yerr=astd, fmt='.-', label='ambient, phase')
+    #plt.plot(gper, gvel, '.-', label='ambient, group')
+    plt.errorbar(gper, gvel, yerr=gstd, fmt='.-', label='ambient, group')
+    #plt.plot(eper, evel, '.-', label='EQ')
+    plt.errorbar(eper, evel, yerr=estd, fmt='.-', label='EQ, phase')
+    plt.legend(fontsize=9)
+    plt.title(stnm[i])
+    plt.xlabel('Period [s]')
+    plt.ylabel('Phase velocity [km/s]')
+    plt.xlim(5,105)
+    #plt.ylim(2.0,4.7)
+    plt.ylim(0.5,4.5)
+    pdf.savefig(fig)
+    plt.close()
+
+pdf.close()
