e39ffadb0e0a573e423aaae16743262eb355beae
Hannah Mark <hannah.f.mark@gmail.com>
2020-10-20 09:59:06 -0500

    second part of YJ??

--- requests/Data_request_1.py
+++ requests/Data_request_1.py
@@ -6,8 +6,8 @@ import os, sys
 # 1P (including permanent stations during same time period)
 # or YJ
 
-stalst = '/P/hmark/ANT_1P/requests/southchile_sta.lst'  # network  station
-#stalst = '/P/hmark/ANT_1P/requests/northchile_sta.lst'  # network  station
+#stalst = '/P/hmark/ANT_1P/requests/southchile_sta.lst'  # network  station
+stalst = '/P/hmark/ANT_1P/requests/northchile_sta.lst'  # network  station
 net,sta = np.loadtxt(stalst,dtype=(str,str),unpack=True)
 
 request_name = 'Hannah Mark'
@@ -15,17 +15,19 @@ request_email = 'hmark@wustl.edu'
 
 #day1 = datetime(2018,11,4)  # first part of 1P
 #dayN = datetime(2019,3,11)
-day1 = datetime(2019,3,11)  # second part of 1P
-dayN = datetime(2019,11,1)  # because I missed half the first time whoops
+#day1 = datetime(2019,3,11)  # second part of 1P
+#dayN = datetime(2019,11,1)  # because I missed half the first time whoops
 #day1 = datetime(2004,12,5) # YJ, or at least part of it (2006/12/31?)
 #dayN = datetime(2006,5,28)
+day1 = datetime(2006,05,29) # trying to get second part of YJ
+dayN = datetime(2006,12,31)
 
 dayR = day1
 while dayR < dayN:
 	BT = dayR
 
-	label = '1P_LH_%s' % (dayR.strftime('%Y.%b.%d'))
-	#label = 'YJ_LH_%s' % (dayR.strftime('%Y.%b.%d'))
+	#label = '1P_LH_%s' % (dayR.strftime('%Y.%b.%d'))
+	label = 'YJ_LH_%s' % (dayR.strftime('%Y.%b.%d'))
 
 	dayR = dayR + timedelta(days=1)
 	ET = dayR
