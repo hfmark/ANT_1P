f40881564c4d5fd8d5b0e2e288bcbbe291da133f
Hannah Mark <hannah.f.mark@gmail.com>
2020-11-02 16:37:19 -0600

    parallel processing steps for tf-pws: cut daily cross-correlations and write causal and acausal halves for stacking

---
+++ to4_cut_daily_xcor.py
@@ -0,0 +1,46 @@
+import numpy as np
+from obspy import read
+from glob import glob
+from datetime import datetime
+import os, sys
+
+####
+# tally up all unique station pairs
+# for each pair:
+    # check that there are at least [N] daily xcor files
+    # if so, loop day files:
+        # read each, slice in half and write positive and reversed-negative as separate files
+        # put those in a directory named for the pair
+####
+
+corfiles = np.array(glob('????.???/COR_D/*/*.SAC'))  # all daily crosscorr files
+pairs = np.array(['_'.join(e.split('/')[-1].split('.')[0].split('_')[1:3]) for e in corfiles])
+u_pairs = np.unique(pairs)
+
+odir = 'COR/'
+
+for p in u_pairs:
+    if p.split('_')[0] == p.split('_')[1]:
+        continue  # skip autocorrelations
+
+    print(p)
+    flist = corfiles[np.where(pairs == p)]  # all the full sym/asym daily crosscorrelation files
+    p_odir = os.path.join(odir,p)
+    os.makedirs(p_odir,exist_ok=True)
+    for i in range(len(flist)):
+        st = read(flist[i])  # read daily xcor file
+
+        st1 = st.copy()  # copy and keep causaul half
+        st1[0].data = st1[0].data[3000:]  # hard-coded to 1/2 assumed length of trace (6001)
+        st2 = st.copy()  # copy and keep acausal half
+        st2[0].data = st2[0].data[::-1]   # reverse
+        st2[0].data = st2[0].data[3000:]  # and trim
+
+        # rewrite halves
+        day_str = '.'.join([flist[i].split('/')[0],flist[i].split('/')[-1].split('.')[0].split('_')[-1]])
+        f_pos = '_'.join([p,day_str]) + '.SACpos'
+        o_pos = os.path.join(p_odir,f_pos)
+        st1.write(o_pos,'SAC')
+        f_neg = '_'.join([p,day_str]) + '.SACneg'
+        o_neg = os.path.join(p_odir,f_neg)
+        st2.write(o_neg,'SAC')
