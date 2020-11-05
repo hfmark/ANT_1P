5eaf9700799f2cdc8b9e285e324bc48e450e28dd
Hannah Mark <hannah.f.mark@gmail.com>
2020-11-05 13:16:43 -0600

    paths again

--- to9_make_disp_curves.py
+++ to9_make_disp_curves.py
@@ -14,14 +14,14 @@ import os, sys
 curves = []
 
 # find all station pairs with stacks
-corfiles = np.array(glob('COR/stacks/tf_*_full.sacn_2_DISP.1'))  # full crosscorr only
+corfiles = np.array(glob('COR/stacks/tf_*_full_hdr.sacn_2_DISP.1'))  # full crosscorr only
 pairs = np.array(['_'.join(e.split('/')[-1].split('_')[1:3]) for e in corfiles])
 inv = read_inventory('seed/dataless/combined.xml')
 
 # loop pairs
 for p in pairs:
     # check: do we have the final aftan outputs? *_full.SAC_2_DISP.1
-    disp2file = 'COR/stacks/tf_%s_full.sacn_2_DISP.1' % p
+    disp2file = 'COR/stacks/tf_%s_full_hdr.sacn_2_DISP.1' % p
     if not os.path.isfile(disp2file):
         print('%s is unfinished' % p)
         continue  # skip this pair
@@ -29,8 +29,8 @@ for p in pairs:
     print(p)
 
     # if so, get that filename, snr filename, and trifile list
-    snrfile = 'COR/stacks/tf_%s_full.sacn_snr.cv.p.txt' % p
-    trifiles = glob('COR/stacks/tf_%s_????.??.????.??.????.??.SAC_2_DISP.1' % p)
+    snrfile = 'COR/stacks/tf_%s_full_hdr.sacn_snr.cv.p.txt' % p
+    trifiles = glob('COR/stacks/tf_%s_????.??.????.??.????.??_hdr.sacn_2_DISP.1' % p)
     sta1 = p.split('_')[0]; sta2 = p.split('_')[1]
 
     # make a dispersion curve and add to list of curves
