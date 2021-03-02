1f5bd483e39431b56535e5582cd74225369f191c
Hannah Mark <hannah.f.mark@gmail.com>
2021-03-02 16:41:14 -0600

    faster coastlines like for EQ tomo

--- rt_ant_comp.py
+++ rt_ant_comp.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import mods.PAT.utils as ut
 import mods.PAT.files as vr
+import cartopy.crs as ccrs
 import ScientificColourMaps6 as SCM6
 import pickle
 import os, sys
@@ -11,15 +12,16 @@ import os, sys
 # read maps from anttomo and RayTomo and compare at one period
 ####
 
-period = 32.
+period = 26.
+m1_thresh = 3.
 
 # set up figure
 plt.ion()
 fig = plt.figure(figsize=(12.3,4.8))
-ax1 = plt.subplot2grid((1,4),(0,0))
-ax2 = plt.subplot2grid((1,4),(0,1))
-ax3 = plt.subplot2grid((1,4),(0,2))
-ax4 = plt.subplot2grid((1,4),(0,3))
+ax1 = plt.subplot2grid((1,4),(0,0),projection=ccrs.PlateCarree())
+ax2 = plt.subplot2grid((1,4),(0,1),projection=ccrs.PlateCarree())
+ax3 = plt.subplot2grid((1,4),(0,2),projection=ccrs.PlateCarree())
+ax4 = plt.subplot2grid((1,4),(0,3),projection=ccrs.PlateCarree())
 
 # read anttomo outputs
 f = open('output/4-pass-tomography_phase.pickle','rb')
@@ -45,7 +47,7 @@ for i in range(dens.shape[0]):
 
 # plot anttomo
 im1 = ax1.imshow(vant.T,\
-		extent=amap.grid.bbox(),
+		extent=amap.grid.bbox(),transform=ccrs.PlateCarree(),
 		origin='lower',cmap=SCM6.roma)
 im1.set_clim(vr.vlims[period])
 
@@ -55,6 +57,8 @@ lon,lat,vph = np.loadtxt('rt_outputs/tomo_iso_phase_3_%i.1' % (period),usecols=(
 # set up triangulation for grid
 xi = np.arange(min(lon),max(lon),0.3)
 yi = np.arange(min(lat),max(lat),0.3)
+xtk = np.arange(-75,-68,3)
+ytk = np.arange(-55,-43,3)
 
 # interpolate velocities
 triang = tri.Triangulation(lon,lat)
@@ -82,7 +86,7 @@ vph_grid.mask[dens_grid < min_dens] = True
 
 # plot isotropic RayTomo
 im2 = ax2.imshow(vph_grid,\
-                extent=[min(xi),max(xi),min(yi),max(yi)],\
+                extent=[min(xi),max(xi),min(yi),max(yi)],transform=ccrs.PlateCarree(),\
 		origin='lower',cmap=SCM6.roma)
 im2.set_clim(vr.vlims[period])
 
@@ -120,19 +124,22 @@ vph_grid.mask[dens_grid < min_dens] = True
 
 # plot anisotropic RayTomo
 im3 = ax3.imshow(vph_grid,\
-                extent=[min(xi),max(xi),min(yi),max(yi)],\
+                extent=[min(xi),max(xi),min(yi),max(yi)],transform=ccrs.PlateCarree(),\
 		origin='lower',cmap=SCM6.roma)
 im3.set_clim(vr.vlims[period])
 
 
 # plot anisotropic RayTomo with arrows
 im4 = ax4.imshow(vph_grid,\
-                extent=[min(xi),max(xi),min(yi),max(yi)],\
+                extent=[min(xi),max(xi),min(yi),max(yi)],transform=ccrs.PlateCarree(),\
 		origin='lower',cmap=SCM6.roma)
 im4.set_clim(vr.vlims[period])
 lon,lat,a_amp,a_psi = np.loadtxt('rt_outputs/tomo_ani_phase_3_%i.1' % (period),\
             usecols=(0,1,5,6),unpack=True)
-ax4.quiver(lon,lat,a_amp*np.cos(np.degrees(a_psi)),a_amp*np.sin(np.degrees(a_psi)))
+lln,llt,m1,m2 = np.loadtxt('rt_outputs/tomo_ani_phase_3_%i.azi' % (period),unpack=True)
+ind = np.where(m1 > m1_thresh)[0]
+ax4.quiver(lon[ind],lat[ind],a_amp[ind],a_amp[ind],angles=(90-a_psi[ind])%360,\
+            transform=ccrs.PlateCarree())
 
 ################################################3
 # fancy up the plots
@@ -141,10 +148,15 @@ cb2 = plt.colorbar(im2,ax=ax2,fraction=0.04,orientation='horizontal')
 cb3 = plt.colorbar(im3,ax=ax3,fraction=0.04,orientation='horizontal')
 cb4 = plt.colorbar(im4,ax=ax4,fraction=0.04,orientation='horizontal')
 
-ut.plot_basemap(ax1,bbox=amap.grid.bbox())
-ut.plot_basemap(ax2,bbox=amap.grid.bbox())
-ut.plot_basemap(ax3,bbox=amap.grid.bbox())
-ut.plot_basemap(ax4,bbox=amap.grid.bbox())
+#ut.plot_basemap(ax1,bbox=amap.grid.bbox())
+#ut.plot_basemap(ax2,bbox=amap.grid.bbox())
+#ut.plot_basemap(ax3,bbox=amap.grid.bbox())
+#ut.plot_basemap(ax4,bbox=amap.grid.bbox())
+for ax in [ax1,ax2,ax3,ax4]:
+    ax.coastlines(resolution='50m')
+    ax.gridlines(crs=ccrs.PlateCarree(),xlocs=xtk,ylocs=ytk)
+    ax.set_xticks(xtk,minor=False,crs=ccrs.PlateCarree())
+    ax.set_yticks(ytk,minor=False,crs=ccrs.PlateCarree())
 
 ax1.set_title('python code')
 ax2.set_title('rt isotropic')
